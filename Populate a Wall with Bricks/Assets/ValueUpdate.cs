af4b2eb924457407433f94cbda256be76bdc3f29
Solmaz <solmazj@princeton.edu>
2017-06-26 17:06:39 -0400

    before messing up with input fields with sliders

---
+++ Populate a Wall with Bricks/Assets/ValueUpdate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ValueUpdate : MonoBehaviour 
+{
+
+	private Slider mySlider;
+	private InputField myField;
+
+	void Start() 
+	{
+		mySlider = gameObject.GetComponent<Slider>();
+		myField = gameObject.GetComponent<InputField>();
+	}
+
+	public void UpdateValueFromFloat(float value) 
+	{
+		Debug.Log("float value changed: " + value);
+		if (mySlider) { mySlider.value = value; }
+		if (myField) { myField.text = value.ToString(); }
+	}
+
+	public void UpdateValueFromString(string value) 
+	{
+		Debug.Log("string value changed: " + value);
+		if (mySlider) { mySlider.value = float.Parse(value); }
+		if (myField) { myField.text = value; }
+	}
+
+
+}
