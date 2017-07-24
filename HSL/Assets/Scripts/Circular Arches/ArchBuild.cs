0884a5b98dde01683b016cb7810b5e3d022e75e2
Solmaz <solmazj@princeton.edu>
2017-07-24 16:51:25 -0400

    Have Odd and Even raws now, would need to work on alternatingly building them

--- HSL/Assets/Scripts/Circular Arches/ArchBuild.cs
+++ HSL/Assets/Scripts/Circular Arches/ArchBuild.cs
@@ -12,22 +12,8 @@ public class ArchBuild : MonoBehaviour {
 	public float archThickness, brickDepth, vaultDepth;
 	int steps = 100;
 	float[] arcLengths;
-	float curveLength,  innerBrickLength;
-	GameObject prefab;
-
-	//how about tagging newly created bricks, and the arch itself
-	public void BuildAVault () {
-		BuildAnArch ();
-		//centering the vault about the origin in z direction
-		GameObject.FindGameObjectWithTag("Arch").transform.position = new Vector3 (GameObject.FindGameObjectWithTag("Arch").transform.position.x,
-			GameObject.FindGameObjectWithTag("Arch").transform.position.y, -vaultDepth/2 + brickDepth);
-		//copy created arch over however many times needed
-		for (int rows = 1; rows < vaultDepth / brickDepth; rows++) {
-			GameObject arch = Instantiate (GameObject.FindGameObjectWithTag("Arch")) as GameObject;
-			arch.transform.position = new Vector3 (arch.transform.position.x, arch.transform.position.y,  arch.transform.position.z + rows * brickDepth);
-		}
-	}
-
+	float curveLength,  innerBrickLength, outerBrickLength;
+	GameObject prefab, halfPrefab;
 
 	public void BuildAnArch () {
 		//stop if the input is missing
@@ -40,7 +26,7 @@ public class ArchBuild : MonoBehaviour {
 		GameObject arch = GameObject.FindGameObjectWithTag ("Arch");
 		if (arch.transform.childCount > 0) {
 			for (int i = 0; i < arch.transform.childCount; i++) {
-				Destroy(arch.transform.GetChild (i).gameObject);
+				Destroy (arch.transform.GetChild (i).gameObject);
 			}
 		}
 
@@ -51,8 +37,41 @@ public class ArchBuild : MonoBehaviour {
 		innerBrickLength = curveLength / numberOfBricks;
 
 		//make prefab bricks given the information about the arch thickness and the arc shape
-		BuildTheBrick ();
-	
+		BuildFullBrick ();
+		OddRaw ();
+	}
+
+
+	public void BuildARegularVault () {
+		BuildAnArch ();
+		//centering the vault about the origin in z direction
+		GameObject.FindGameObjectWithTag("Arch").transform.position = new Vector3 (GameObject.FindGameObjectWithTag("Arch").transform.position.x,
+			GameObject.FindGameObjectWithTag("Arch").transform.position.y, -vaultDepth/2 + brickDepth);
+		//copy created arch over however many times needed
+		for (int rows = 1; rows < vaultDepth / brickDepth; rows++) {
+			GameObject arch = Instantiate (GameObject.FindGameObjectWithTag("Arch")) as GameObject;
+			arch.transform.position = new Vector3 (arch.transform.position.x, arch.transform.position.y,  arch.transform.position.z + rows * brickDepth);
+		}
+	}
+
+
+	public void BuildLongZipVault () {
+		BuildAnArch ();
+		BuildHalfBrick ();
+		//centering the vault about the origin in z direction
+		GameObject.FindGameObjectWithTag("Arch").transform.position = new Vector3 (GameObject.FindGameObjectWithTag("Arch").transform.position.x,
+			GameObject.FindGameObjectWithTag("Arch").transform.position.y, -vaultDepth/2 + brickDepth);
+//		for (int rows = 1; rows < vaultDepth / brickDepth; rows++) {
+			GameObject evenArch = Instantiate (GameObject.FindGameObjectWithTag("Arch")) as GameObject;
+		for (int i = 0; i < evenArch.transform.childCount; i++) {
+			Destroy (evenArch.transform.GetChild (i).gameObject);
+		}
+		EvenRaw ();
+//		}
+	}
+
+
+	void OddRaw () {
 		//parameter u for the arc length is like t for the curve; it's between 0 and 1
 		float u = (innerBrickLength/2)/curveLength;
 		for (int i = 1; i <= numberOfBricks; i++) {
@@ -65,6 +84,38 @@ public class ArchBuild : MonoBehaviour {
 			item.transform.parent = transform;
 			u += innerBrickLength / curveLength;
 		}
+	}
+
+
+	void EvenRaw () {
+		//parameter u for the arc length is like t for the curve; it's between 0 and 1
+		float u = (innerBrickLength/4)/curveLength;
+		Transform item = Instantiate (halfPrefab.transform) as Transform;
+		Vector3 position = wireArc.GetPoint (Map (u));
+		item.transform.localPosition = position;
+		//align the bricks along the arc
+		item.transform.LookAt (position + wireArc.GetDirection (u));
+		//setting the object to which this script is attached as the parent of the created bricks
+		item.transform.parent = transform;
+		u = u + (3 * (innerBrickLength/curveLength) / 4);
+		for (int i = 1; i < numberOfBricks; i++) {
+			item = Instantiate (prefab.transform) as Transform;
+			position = wireArc.GetPoint (Map (u));
+			item.transform.localPosition = position;
+			//align the bricks along the arc
+			item.transform.LookAt (position + wireArc.GetDirection (u));
+			//setting the object to which this script is attached as the parent of the created bricks
+			item.transform.parent = transform;
+			u += innerBrickLength / curveLength;
+		}
+		u = u - ((innerBrickLength / curveLength) / 4);
+		item = Instantiate (halfPrefab.transform) as Transform;
+		position = wireArc.GetPoint (Map (u));
+		item.transform.localPosition = position;
+		//align the bricks along the arc
+		item.transform.LookAt (position + wireArc.GetDirection (u));
+		//setting the object to which this script is attached as the parent of the created bricks
+		item.transform.parent = transform;
 	}
 
 			
@@ -88,7 +139,7 @@ public class ArchBuild : MonoBehaviour {
 	}
 
 
-	void BuildTheBrick () {
+	void BuildFullBrick () {
 		//calculating the outer length of the brick
 		float t1 = (innerBrickLength) / curveLength;
 		float t2 = (2 * innerBrickLength) / curveLength;
@@ -96,7 +147,7 @@ public class ArchBuild : MonoBehaviour {
 		Vector3 tangent2 = wireArc.GetDirection (t2);
 		Vector3 scaled1 = new Vector3 (-archThickness * tangent1.y, archThickness *  tangent1.x, tangent1.z);
 		Vector3 scaled2 = new Vector3 (-archThickness * tangent2.y, archThickness *  tangent2.x, tangent2.z);
-		float outerBrickLength = Vector3.Distance (wireArc.GetPoint (t1) + scaled1, wireArc.GetPoint (t2) + scaled2);
+		outerBrickLength = Vector3.Distance (wireArc.GetPoint (t1) + scaled1, wireArc.GetPoint (t2) + scaled2);
 
 		//create empty prefab, and destroy the empty gameobject
 		GameObject sampleBrick = new GameObject("SampleBrick");
@@ -106,6 +157,27 @@ public class ArchBuild : MonoBehaviour {
 		WedgeBrickMesh mesh = prefab.AddComponent<WedgeBrickMesh> ();
 		//string Brick can be made into a parameter too, defines the texture
 		mesh.CreateMesh (innerBrickLength, outerBrickLength, archThickness, brickDepth, "Brick");
+	}
+
+
+	void BuildHalfBrick () {
+		//calculating the outer length of the brick
+		float t1 = (innerBrickLength) / curveLength;
+		float t2 = (2 * innerBrickLength) / curveLength;
+		Vector3 tangent1 = wireArc.GetDirection (t1);
+		Vector3 tangent2 = wireArc.GetDirection (t2);
+		Vector3 scaled1 = new Vector3 (-archThickness * tangent1.y, archThickness *  tangent1.x, tangent1.z);
+		Vector3 scaled2 = new Vector3 (-archThickness * tangent2.y, archThickness *  tangent2.x, tangent2.z);
+		outerBrickLength = Vector3.Distance (wireArc.GetPoint (t1) + scaled1, wireArc.GetPoint (t2) + scaled2);
+
+		//create empty prefab, and destroy the empty gameobject
+		GameObject sampleBrick = new GameObject("SampleBrick");
+		halfPrefab = PrefabUtility.CreatePrefab ("Assets/Resources/HalfWedgedBrick.prefab", sampleBrick);
+		Destroy (sampleBrick); 
+		//add the mesh component
+		WedgeBrickMesh mesh = halfPrefab.AddComponent<WedgeBrickMesh> ();
+		//string Brick can be made into a parameter too, defines the texture
+		mesh.CreateMesh (innerBrickLength/2, outerBrickLength/2, archThickness, brickDepth, "Brick");
 	}
 
 
